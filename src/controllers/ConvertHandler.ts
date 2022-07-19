896a1a327a563d1fadb63ed177b9ad0b44d057b1
Diogo Jorge <diogojorge1401@gmail.com>
2022-07-19 16:57:53 -0300

    refactor: return l in uppercase

--- src/controllers/ConvertHandler.ts
+++ src/controllers/ConvertHandler.ts
@@ -46,7 +46,7 @@ export default class ConvertHandler {
     result = result[1] ?? result[0]
     if (!this.isValidUnit(result))
       result = 'invalid unit'
-    return result;
+    return result === 'l' ? 'L' : result;
   };
 
   getReturnUnit(initUnit: string) {
